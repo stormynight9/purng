d1431661dac3ec9c880cd8de2eb17dd234e1245a
stormynight9 <ferjani.nader@hotmail.fr>
2025-01-01 15:49:17 +0100

    chore: config files

---
+++ .prettierrc.mjs
@@ -0,0 +1,12 @@
+/** @type {import("prettier").Config} */
+const config = {
+    plugins: ['prettier-plugin-tailwindcss'],
+    trailingComma: 'es5',
+    singleQuote: true,
+    jsxSingleQuote: true,
+    tabWidth: 4,
+    printWidth: 80,
+    endOfLine: 'lf',
+};
+
+export default config;
